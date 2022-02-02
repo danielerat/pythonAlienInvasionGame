14436fc71c2869e77c02d8a5a6d31dbde4c2498d
danielerat <davidodo2015@gmail.com>
2022-02-02 17:47:21 +0200

    Apply the same moving to fix the right side

--- ship.py
+++ ship.py
@@ -28,7 +28,7 @@ class Ship:
 
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x+=self.settings.ship_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.x-=self.settings.ship_speed
             
         # Update rect object from self.x.
