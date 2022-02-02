f83a2be10903704b3a29c0f87420340fb6f11786
danielerat <davidodo2015@gmail.com>
2022-02-02 07:31:22 +0200

    Making The Files Executable to run them well, (Annoying Linux

--- test.py
+++ test.py
@@ -6,22 +6,25 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the dame and create game resources."""
         pygame.init()
+        
         self.screen=pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
         
-        def run_game(self):
-            """Start the main loop for the game."""
-            while True:
-                #Whatch for keyboard and mouse events.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-                
-                #Make the most recently Drawn screen visible    
-                pygame.display.flip()
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            #Whatch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            #Make the most recently Drawn screen visible    
+            pygame.display.flip()
+
 
 if __name__=="__main__":
     #Make a game instance and run the game
+    print("LEt's Rock")
     ai=AlienInvasion()
     ai.run_game()
         
