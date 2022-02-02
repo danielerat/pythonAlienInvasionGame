c7ac125c21bcae0a1897eefb939cb8b30b407021
danielerat <davidodo2015@gmail.com>
2022-02-02 17:56:34 +0200

    Refactoring the key up and key down event and add them in their own helper

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,17 +35,10 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
                     
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False 
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False 
+                self._check_keyup_events(event) 
                     
     def _update_screen(self):
         """Update Images ON the screen, and flip to the new Screen"""
@@ -55,6 +48,18 @@ class AlienInvasion:
         self.ship.blitme()
         #Make the most recently Drawn screen visible    
         pygame.display.flip()
+    def _check_keydown_events(self,event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False 
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 if __name__=="__main__":
     #Make a game instance and run the game
