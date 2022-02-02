54a0a01404492c786a0d6b9cb654961434d51be8
danielerat <davidodo2015@gmail.com>
2022-02-02 19:07:18 +0200

    Adding a mechanism of destroying bullets once a user is done firing

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,9 +31,15 @@ class AlienInvasion:
             
             self._check_events()
             self.ship.update()
-            self._update_screen()
-            
             self.bullets.update()
+            
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+                        
+            self._update_screen()
         
     def _check_events(self):
         # This is a helper method , they all start with _
